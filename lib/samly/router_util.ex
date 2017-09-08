e3cf0c21d276ef0412ca4f69697004f4cd8298e8
handnot2 <handnot2@gmail.com>
2017-09-08 15:15:07 -0700

    send SAML requests by POST

--- lib/samly/router_util.ex
+++ lib/samly/router_util.ex
@@ -4,17 +4,12 @@ defmodule Samly.RouterUtil do
   alias Plug.Conn
 
   def send_saml_request(conn, idp_url, signed_xml_payload, relay_state) do
-    import :esaml_binding, only: [encode_http_redirect: 4, encode_http_post: 3]
+    import :esaml_binding, only: [encode_http_post: 3]
 
-    ie? = Conn.get_req_header(conn, "user-agent") == ["MSIE"]
-
-    saml_url = encode_http_redirect(idp_url, signed_xml_payload, "n/a", relay_state)
-    if ie? && byte_size(saml_url) > 2042 do
-      resp_body = encode_http_post(idp_url, signed_xml_payload, relay_state)
-      conn |> Conn.send_resp(200, resp_body)
-    else
-      conn |> redirect(302, saml_url)
-    end
+    resp_body = encode_http_post(idp_url, signed_xml_payload, relay_state)
+    conn
+    |>  Conn.put_resp_header("Content-Type", "text/html")
+    |>  Conn.send_resp(200, resp_body)
   end
 
   def redirect(conn, status_code, dest) do
