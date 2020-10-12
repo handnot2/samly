be5a42254fd4e7372935b97e7018902574c01981
Kamil Zieliński <paryzowy@gmail.com>
2020-10-12 18:48:01 +0200

    Use own esaml

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule Samly.Mixfile do
   defp deps() do
     [
       {:plug, "~> 1.6"},
-      {:esaml, "~> 4.2"},
+      {:esaml, github: "Recruitee/esaml", branch: "main"},
       {:sweet_xml, "~> 0.6.6"},
       {:ex_doc, "~> 0.19.0", only: :dev, runtime: false},
       {:inch_ex, "~> 1.0", only: [:dev, :test]}
